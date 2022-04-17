5932369f0d138e4f84d78805cf53d7dbf54b1171
indra <indrajit.das@ivanwebsolutions.com>
2022-04-18 03:28:08 +0530

    17.04.2022

---
+++ app/src/main/java/com/example/govyshcar/Resetpassword.kt
@@ -0,0 +1,11 @@
+package com.example.govyshcar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Resetpassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resetpassword)
+    }
+}
